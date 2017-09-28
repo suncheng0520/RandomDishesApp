3772117fe968422a0fa76636383fe130583fca97
Sam Lee <shangcheng0520@gmail.com>
2017-09-28 13:48:18 +0800

    新增控制項

--- RandomDishesApp/Form1.cs
+++ RandomDishesApp/Form1.cs
@@ -21,5 +21,15 @@ namespace RandomDishesApp
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
